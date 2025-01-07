865f2238a9cce1badceee5cc2e22f3a16c6efe25
thegera4 <84020433+thegera4@users.noreply.github.com>
2025-01-07 15:50:21 -0600

    update: finished main app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Basic TODO Full Stack application with the stack HTMX, Go and MySQL. 
+
+![Todo app](image.png)
--- main.go
+++ main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
-// Global variables
+/*** Global variables ***/
 
 var db *sql.DB // This variable stores the database connection object
 var tmpl *template.Template // This variable stores the parsed templates
 
-// Custom Types
+/*** Custom Types ***/
 
 // Custom type for a TODO
 type Task struct {
@@ -48,10 +52,21 @@ func main() {
 	gRouter := mux.NewRouter()
 
 	gRouter.HandleFunc("/", HomeHandler)
+
 	gRouter.HandleFunc("/tasks", fetchTasks).Methods("GET")
+	gRouter.HandleFunc("/tasks", addTask).Methods("POST")
+	gRouter.HandleFunc("/tasks/{id}", updateTask).Methods("PUT", "POST")
+	gRouter.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
+
+	gRouter.HandleFunc("/newTaskForm", getTaskForm).Methods("GET")
+
+	gRouter.HandleFunc("/taskUpdateForm/{id}", getTaskUpdateForm).Methods("GET")
+
 	
 	http.ListenAndServe(":8080", gRouter)
 }
+
+/*** Handler functions ***/
 
 // This handler function sends a response to the root URL ("/")
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -66,6 +81,93 @@ func fetchTasks(w http.ResponseWriter, r *http.Request) {
 	todos, _ := getTasks(db)
 	tmpl.ExecuteTemplate(w, "todoList", todos)
 }
+
+// This handler function is used to render the form to create a new task
+func getTaskForm(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "addTaskForm", nil)
+}
+
+// This handler function is used to add a new task to the Database
+func addTask(w http.ResponseWriter, r *http.Request) {
+	task := r.FormValue("task") // task is the name of the input field that we want to get the value from
+	query := "INSERT INTO tasks (task) VALUES (?)"
+	
+	stmt, err := db.Prepare(query)
+	if err != nil { log.Fatal(err) }
+
+	defer stmt.Close()
+
+	_, executeErr := stmt.Exec(task)
+	if executeErr != nil { log.Fatal(executeErr) }
+
+	todos, _ := getTasks(db)
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+// This handler function is used to render the form to update a task
+func getTaskUpdateForm(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	taskId, _ := strconv.Atoi(vars["id"])
+	task, err := getTaskById(db, taskId)
+	if err != nil { http.Error(w, err.Error(), http.StatusInternalServerError) }
+
+	tmpl.ExecuteTemplate(w, "updateTaskForm", task)
+}
+
+// This handler function is used to update a task in the Database. Reloads the updated list of tasks if rows were affected.
+func updateTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Get the URL variables
+	taskItem := r.FormValue("task") // task is the name of the input field that we want to get the value from
+	isDone := r.FormValue("done") // done is the name of the input field that we want to get the value from
+	
+	var taskStatus bool
+
+	switch strings.ToLower(isDone) {
+		case "yes", "on":
+			taskStatus = true
+		case "no", "off":
+			taskStatus = false
+		default:
+			taskStatus = false
+	}
+
+	taskId, _ := strconv.Atoi(vars["id"])
+	task := Task{taskId, taskItem, taskStatus}
+
+	query := "UPDATE tasks SET task = ?, done = ? WHERE id = ?"
+	result, err := db.Exec(query, task.Description, task.Done, task.Id)
+	if err != nil { log.Fatal(err) }
+
+	rowsAffected, _ := result.RowsAffected()
+	if rowsAffected == 0 {
+		http.Error(w, fmt.Sprintf("Task with ID %d not found", taskId), http.StatusNotFound)
+		return
+	}
+
+	// Return the fresh list of tasks if rows were affected
+	todos, _ := getTasks(db)
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+// This handler function is used to delete a task from the Database. Reloads the updated list of tasks if rows were affected.
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // Get the URL variables
+	taskId, _ := strconv.Atoi(vars["id"])
+
+	query := "DELETE FROM tasks WHERE id = ?"
+	stmt, err := db.Prepare(query)
+	if err != nil { log.Fatal(err) }
+	defer stmt.Close()
+
+	_, deleteErr := db.Exec(query, taskId)
+	if deleteErr != nil { log.Fatal(deleteErr) }
+
+	// Return the fresh list of tasks if no errors occurred
+	todos, _ := getTasks(db)
+	tmpl.ExecuteTemplate(w, "todoList", todos)
+}
+
+/*** Utility functions ***/
 
 // This utility function makes a query to the DB to get all data saved in the "tasks" table: (SELECT * FROM tasks)
 func getTasks(dbPointer *sql.DB) ([]Task, error) {
@@ -88,4 +190,21 @@ func getTasks(dbPointer *sql.DB) ([]Task, error) {
 	}
 
 	return tasks, nil
+}
+
+// This utility function makes a query to the DB to get a single task by its ID
+func getTaskById(dbPointer *sql.DB, id int) (*Task, error) {
+	query := "SELECT * FROM tasks WHERE id = ?"
+	row := dbPointer.QueryRow(query, id)
+
+	var todo Task
+	err := row.Scan(&todo.Id, &todo.Description, &todo.Done)
+	if err != nil { 
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("Task with ID %d not found", id)
+		}
+		return nil, err 
+	}
+
+	return &todo, nil
 }
